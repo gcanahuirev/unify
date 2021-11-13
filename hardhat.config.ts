e7282c0227674589503db4c1dbec5704b0a0bf81
Giuseppe <giuseppecv56@gmail.com>
2021-11-12 20:56:48 -0500

    test: add testing for smart contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,25 +3,14 @@ import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 
-import { HardhatUserConfig, task } from 'hardhat/config';
-
-task('accounts', 'Prints the list of accounts', async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  let index = 1;
-
-  accounts.map(async (account) => {
-    const item = await account.getAddress();
-    console.log(`account ${index}:`, item);
-    index++;
-  });
-});
+import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   solidity: '0.8.4',
   networks: {
     goerli: {
-      url: process.env.GOERLI_URL,
+      url: `${process.env.GOERLI_URL}`,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
